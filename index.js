3cff927f3dcc0ff4c7449f93012b112aff5424eb
Arman Sargsyan <11arman@bk.ru>
2024-04-25 10:30:07 -0800

    again new function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log(1);
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
