435e5e1fe7ee872cac6512fd6b04338cc7265868
Arman Sargsyan <11arman@bk.ru>
2024-04-25 10:30:51 -0800

    try 2. throw to github

--- index.js
+++ index.js
@@ -4,12 +4,9 @@ for (let index = 0; index < array.length; index++) {
     
 }
 
-import React from 'react'
 
 const index = () => {
-  return (
-    <div>index</div>
-  )
+  return 5
 }
 
 export default index
