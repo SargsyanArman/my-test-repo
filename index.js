41b95ab4dfb40e58335427c5de6950b24bcf0cd0
Arman Sargsyan <11arman@bk.ru>
2024-04-29 04:23:13 -0800

    first added index.js

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
